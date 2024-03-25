69cc9c79975313a8d5d56a6aede3af883f5985cd
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-03-25 08:19:00 +0530

    updates

--- band_pass.py
+++ band_pass.py
@@ -2,17 +2,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Load the image
 img = cv2.imread('assets/cat.jpeg', 0)
 
 if img is None:
     print("Error loading the image.")
 else:
-    # Apply unsharp masking for sharpening
     blurred_img = cv2.GaussianBlur(img, (0, 0), 2.0)
     sharpened_img = cv2.addWeighted(img, 1.5, blurred_img, -0.5, 0)
 
-    # Display the original and sharpened images
     plt.subplot(121), plt.imshow(img, cmap='gray')
     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
     plt.subplot(122), plt.imshow(sharpened_img, cmap='gray')
--- band_reject.py
+++ band_reject.py
@@ -2,39 +2,31 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Load the image
 img = cv2.imread('assets/cat.jpeg', 0)
 
 if img is None:
     print("Error loading the image.")
 else:
-    # Generate Gaussian noise with zero mean and specified sigma
     mean = 0
-    sigma = 15  # Adjust sigma to control the noise level
+    sigma = 15  
     rows, cols = img.shape
     noise = np.random.normal(mean, sigma, (rows, cols))
 
-    # Add the noise to the image
     noisy_img = img.astype(np.float32) + noise.astype(np.float32)
 
-    # Ensure the pixel values are within the valid range
     noisy_img = np.clip(noisy_img, 0, 255)
     noisy_img = noisy_img.astype(np.uint8)
 
-    # Fourier transform
     f = np.fft.fft2(noisy_img)
     fshift = np.fft.fftshift(f)
 
-    # Define the band reject filter
     crow, ccol = rows // 2, cols // 2
     fshift[crow - 30:crow + 30, ccol - 30:ccol + 30] = 0
 
-    # Inverse Fourier transform
     f_ishift = np.fft.ifftshift(fshift)
     img_back = np.fft.ifft2(f_ishift)
     img_back = np.abs(img_back)
 
-    # Display the original, noisy, and filtered images
     plt.subplot(131), plt.imshow(img, cmap='gray')
     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
     plt.subplot(132), plt.imshow(noisy_img, cmap='gray')
